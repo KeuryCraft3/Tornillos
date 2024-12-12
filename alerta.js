c67ba47b1b5697443ddab9a1ffe9a665bf4b4546
KeuryCraft3 <KeuryCraft@gmail.com>
2024-12-12 15:21:46 -0600

    initial commit

---
+++ alerta.js
@@ -0,0 +1,80 @@
+window.addEventListener("load", function() {
+
+document.getElementById("compra").addEventListener("click", function() {
+
+    alert("Gracias por su compra, precio: 500$")
+})
+
+})
+
+window.addEventListener("load", function() {
+
+    document.getElementById("compra2").addEventListener("click", function() {
+    
+        alert("Gracias por su compra, precio: 300$")
+    })
+    
+    })
+
+    window.addEventListener("load", function() {
+
+        document.getElementById("compra3").addEventListener("click", function() {
+        
+            alert("Gracias por su compra, precio: 350$")
+        })
+        
+        })
+
+        window.addEventListener("load", function() {
+
+            document.getElementById("compraV1").addEventListener("click", function() {
+            
+                alert("Gracias por su compra, precio: 850$")
+            })
+            
+            })
+
+            window.addEventListener("load", function() {
+
+                document.getElementById("compraV2").addEventListener("click", function() {
+                
+                    alert("Gracias por su compra, precio: 500$")
+                })
+                
+                })
+
+                window.addEventListener("load", function() {
+
+                    document.getElementById("compraV3").addEventListener("click", function() {
+                    
+                        alert("Gracias por su compra, precio: 1,200$")
+                    })
+                    
+                    })
+
+                    window.addEventListener("load", function() {
+
+                        document.getElementById("compraV4").addEventListener("click", function() {
+                        
+                            alert("Gracias por su compra, precio: 300$")
+                        })
+                        
+                        })
+
+                        window.addEventListener("load", function() {
+
+                            document.getElementById("compraV5").addEventListener("click", function() {
+                            
+                                alert("Gracias por su compra, precio: 600$")
+                            })
+                            
+                            })
+
+                            window.addEventListener("load", function() {
+
+                                document.getElementById("compraV6").addEventListener("click", function() {
+                                
+                                    alert("Gracias por su compra, precio: 800$")
+                                })
+                                
+                                })
